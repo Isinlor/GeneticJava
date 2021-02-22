6238cb3a399727bd132befb279e0e3f822d8ff91
Laura Isotalo <l.isotalo@student.maastrichtuniversity.nl>
2021-02-22 17:18:21 +0100

    Implement population

--- src/Contract/Population.java
+++ src/Contract/Population.java
@@ -1,4 +1,23 @@
 package Contract;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Population<T> {
+    private Set<T> individuals;
+    private int populationSize;
+    public Population(Set<T> individuals) {
+        this.individuals = individuals;
+        this.populationSize = individuals.size();
+    }
+
+    public Set<T> getIndividuals() {
+        Set<T> copy = new HashSet<>();
+        copy.addAll(this.individuals);
+        return copy;
+    }
+
+    public int getSize() {
+        return populationSize;
+    }
 }
